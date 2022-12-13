d6265e2c9d9d337e6cf28482660fdc0b148c1712
CCC621 <ec98721zzz@gmail.com>
2022-12-13 12:13:54 +0900

    コメントの追加

--- web-server/html/connect.php
+++ web-server/html/connect.php
@@ -1,4 +1,8 @@
 <?php 
+
+    //mysql8.0以前のクライアントは「caching_sha2_password」という認証プラグインがサポートされていない為、アクセスできません
+    //mysql -h 127.0.0.1 -P 4306 -u user -ppassword db
+
     //dbへの接続確認用
     try {
         $dsn = 'mysql:host=db;dbname=db;charset=utf8';
