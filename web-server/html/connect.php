5c59dc42b4091cc16df1a14791bd177aa65e99a0
CCC621 <ec98721zzz@gmail.com>
2022-12-13 11:38:20 +0900

    動作確認用のファイル追加

---
+++ web-server/html/connect.php
@@ -0,0 +1,18 @@
+<?php 
+    //dbへの接続確認用
+    try {
+        $dsn = 'mysql:host=mysql;dbname=db;charset=utf8';
+        $db = new PDO($dsn, 'user', 'password');
+
+        $sql = 'SELECT version();';
+        $contact = $db->prepare($sql);
+        $contact->execute();
+        
+        $result = $contact->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($result);
+
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+
+?>
